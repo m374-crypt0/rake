ec9c615aab6368995325b2c79d37b635d9b6eab4
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:43:46 +0000

    docs: help system clearer (hopefully)

--- scripts/help.sh
+++ scripts/help.sh
@@ -2,21 +2,52 @@
 
 main() {
   cat <<EOF
-rake: redone with make
+rake: redone with make - the utility your mono-repo deserves
+
+  Here is the power of rake, you can specify a list of targets separated by
+  spaces and execute them seamlessly.
 
 usage:
+  make <COMPOUND TARGETS> where <COMPOUND TARGETS> is a list of valid make
+  targets built as following:
 
-make <SIMPLE TARGET> where <SIMPLE TARGET> belongs to:
+    make <SUB> <TARGET1> <TARGET2> ... <TARGETN> where <SUB> is a sub-directory
+    located in the special 'subs' directory of rake. A valid <SUB> must contain
+    a valid Makefile having the targets you have specified in
+    <COMPOUND TARGETS>
 
-- init_submodules: init required git submodules (testing and dependencies)
-- help: print this message
-- run_rake_tests: run all test suites
-- watch_rake_tests: continuously run all test suites (for dev purposes)
+examples:
+  Let's say you have a 'backend' and a 'frontend' sub in subs:
 
-Aditionally, you can provide <COMPOUND TARGETS> to this make project, that is,
-several words targets such as:
+    make backend build test deploy
 
-- sub list: list existing subs
+  It will:
+  - build the backend
+  - if successful test it
+  - if successful deploy it
+
+  Then you could:
+
+    make frontend lint transpile deploy
+
+  It will:
+  - lint the frontend
+  - if successful transpile it
+  - if successful deploy it
+
+  Everything at the root of rake, simple, obvious, idiot and boring.
+
+contribution:
+
+  Aditionally, you can provide <SIMPLE TARGETS> to this make project.
+  These targets are specific to rake and aim at either:
+    - providing some help:
+      - make
+      - make help
+    - contribution purposes:
+      - run_rake_tests: run all test suites
+      - watch_rake_tests: continuously run all test suites
+      - init_submodules: init required git submodules
 EOF
 }
 
