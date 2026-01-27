24907b3b1995651ca149cfa3393ded9c01d04f52
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:43:46 +0000

    test: renamed suites

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-"${RAKE_ROOT_DIR}"test/bats/bin/bats -pr "${RAKE_ROOT_DIR}"test/suites/main_makefile/*.bats \
-  "${RAKE_ROOT_DIR}"test/suites/unit/*.bats
+"${RAKE_ROOT_DIR}"test/bats/bin/bats -pr "${RAKE_ROOT_DIR}"test/suites/Makefile/*.bats \
+  "${RAKE_ROOT_DIR}"test/suites/scripts/*.bats
